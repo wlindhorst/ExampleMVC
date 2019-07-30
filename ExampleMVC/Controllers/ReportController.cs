98a5eb415292f6715ccf6a6b584366a99ded79f2
William Lindhorst <lindhorst@sbcglobal.net>
2019-07-30 07:53:02 -0500

    Initial commit

---
+++ ExampleMVC/Controllers/ReportController.cs
@@ -0,0 +1,39 @@
+﻿using ExampleMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ExampleMVC.Controllers
+{
+    public class ReportController : Controller
+    {
+        // GET: Report
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult NewUserForm()
+        {
+            return View("User");
+        }
+
+        [HttpPost]
+        public ViewResult NewUserForm(UserModel usermodel)
+        {
+            if (ModelState.IsValid)
+            {
+                usermodel.Success = true;
+                return View("UserSubmitConfirm", usermodel);
+            }
+            else
+            {
+                usermodel.Success = false;
+                return View("User", usermodel);
+            }
+        }
+    }
+}
---
+++ ExampleMVC/Models/UserModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ExampleMVC.Models
+{
+    public class UserModel
+    {
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public bool Success { get; set; }
+        public string MailingList { get; set; }
+
+    }
+}
